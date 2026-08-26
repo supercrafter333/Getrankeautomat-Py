303c4d1fcb8660598ce228f2d88b8b84149e3215
Christoph Regensburger <justcoolstuff99@gmail.com>
2026-08-26 08:20:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -109,6 +109,21 @@ side_frame = ttk.Frame(
 side_frame.pack(side=tk.LEFT, padx=(20, 20))
 side_frame.pack_propagate(False)
 
+# Grid mit 4 Zeilen konfigurieren (weight=1 sorgt für gleichmäßige Aufteilung)
+for row_idx in range(4):
+    side_frame.grid_rowconfigure(row_idx, weight=1)
+side_frame.grid_columnconfigure(0, weight=1)
+
+# Elemente in den 4 Zeilen platzieren
+geldAnzeige = ttk.Label(side_frame, text="Eingeworfener Betrag: 00,00€")
+geldAnzeige.grid(row=0, column=0, sticky="w")
+
+
+
+test2 = ttk.Label(side_frame, text="Test")
+test2.grid(row=1, column=0, sticky="w")
+
+
 getraenke = load_json()
 create_drink_buttons(getraenke)
 
